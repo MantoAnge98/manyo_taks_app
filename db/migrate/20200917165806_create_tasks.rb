bb16bdab2b9c52406cdac0adfa173e5ec1790598
Ange <angepop1998@gmail.com>
2020-09-18 09:09:17 +0000

    Change column not null to Task database

--- db/migrate/20200917165806_create_tasks.rb
+++ db/migrate/20200917165806_create_tasks.rb
@@ -1,8 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :name, null: false, default: ""
-      t.text :detail, null: false, default: ""
+      t.string :name
+      t.text :detail
 
       t.timestamps
     end
