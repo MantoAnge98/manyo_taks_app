5ef5c0721655a075c7e2bc177329eb8b7c20fe7c
Ange <angepop1998@gmail.com>
2020-09-18 09:24:18 +0000

    prevent uneccessary file in Config folder

--- config/application.rb
+++ config/application.rb
@@ -13,12 +13,12 @@ module ManyoTaskApp
 
     config.generators do |g|
       g.test_framework :rspec,
-                       model_specs: true,
-                       view_specs: false,
-                       helper_specs: false,
-                       routing_specs: false,
-                       controller_specs: false,
-                       request_specs: false
+        model_specs: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
     end
   end
 end
