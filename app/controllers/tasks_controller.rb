2ea099768bad675887dc1773ccf468597f47642e
Ange <angepop1998@gmail.com>
2020-09-21 17:54:07 +0000

    Add status properties in controller and model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -68,7 +68,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:name, :detail, :deadline)
+    params.require(:task).permit(:name, :detail, :deadline, :status)
   end
   
   def set_task
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 80 }
   validates :detail, presence: true, length: { maximum: 250 }
   validates :deadline, presence: true
+  validates :status, presence: true
 end
