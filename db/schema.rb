3c7c135a9a717d38df3b54f0aca2093e8c426d2e
Ange <angepop1998@gmail.com>
2020-09-21 14:22:07 +0000

    Add colum not null to deadline in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_114110) do
+ActiveRecord::Schema.define(version: 2020_09_21_140423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_09_21_114110) do
     t.text "detail", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "deadline"
+    t.date "deadline", null: false
   end
 
 end
