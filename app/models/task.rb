aa354195d90f00a999259ab591d3fe459688d36c
Ange <angepop1998@gmail.com>
2020-09-21 13:42:19 +0000

    Add validadates to deadline propertie

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 80 }
   validates :detail, presence: true, length: { maximum: 250 }
-
+  validates :deadline, presence: true
 end
