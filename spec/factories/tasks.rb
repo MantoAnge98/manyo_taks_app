3e20d0afe8f942a55f26d51f97ba9f6c1c5b31ef
Ange <angepop1998@gmail.com>
2020-09-22 15:44:32 +0000

    Add to spec folder Task test

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -6,6 +6,8 @@ FactoryBot.define do
     name { 'Default title 1 created in the Factory' }
     detail { 'Default content 1 created by Factory' }
     deadline {DateTime.now}
+    status {'completed'}
+    priority {'hight'}
   end
   # Name the test data to be created as "second_task
   # (To name a class name that does not exist, specify the option "Test data for this class")
@@ -13,11 +15,15 @@ FactoryBot.define do
     name { 'Default title 2 created in the Factory' }
     detail { 'Default content 2 created by Factory' }
     deadline {DateTime.tomorrow}
+    status {'not_yet'}
+    priority {'middle'}
   end
 
   factory :third_task, class: Task do
     name { 'Default title 3 created in the Factory' }
     detail { 'Default content 3 created by Factory' }
     deadline {DateTime.now.since(5.days)}
+    status {'in_progress'}
+    priority {'low'}
   end
 end 
