bf838181345652cbe38751adcdd89ad696dd4192
Ange <angepop1998@gmail.com>
2020-09-22 17:21:33 +0000

    Add to model task the function to enum meaning of number of status and priority, and add function to search any variable

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,14 @@ class Task < ApplicationRecord
   validates :deadline, presence: true
   validates :status, presence: true
   validates :priority, presence: true
+  #Define Fucntion to search answer in databse
+  #name is string so function scope is
+  scope :name_search, -> (params) { where('(name LIKE ?)', "#{[:task][:name]}")}
+  #status is integer so function scope is
+  scope :status_search, -> (params) { where(status: params[:task][:status])}
+  #Define enum function
+  #status is integer but we want to see the meaning of each number
+  enum status: {not_yet: 0, in_progress: 1, completed: 2}
+  #prority is integer but we want to see the meaning of each number
+  enum priority: {high: 0, middle: 1, low: 2}
 end
