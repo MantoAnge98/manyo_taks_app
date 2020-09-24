b53887538e672a55bb3a7720f95b0b742b2cd341
Ange <angepop1998@gmail.com>
2020-09-24 22:09:54 +0000

    Add validates function to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   has_secure_password
   #Transform Uppercase to lowercase before save
   before_validation {email.downcase}
+  #Add validates function before go to databse
+  validates :name, presence: true, length: {maximum: 30}
+  validates :email, presence: true, length: {maximum: 255},
+                                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :password, presence: true, length: {minimum: 6}
 end
