91580ede27ea1d9a86f5ffef0601c04c8c49fa3d
Ange <angepop1998@gmail.com>
2020-09-24 22:02:40 +0000

    Add has many to user model and function to secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :tasks, dependent: :destroy
+  has_secure_password
+  #Transform Uppercase to lowercase before save
+  before_validation {email.downcase}
 end
