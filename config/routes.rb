55dd5ad60f354d64f86b7f006c4db1cbcb67fef7
Ange <angepop1998@gmail.com>
2020-09-24 23:55:19 +0000

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'users/index'
+  root to: "public#homepage"
+  resources :users, only: [:new, :create, :edit, :update, :show]
+  resources :sessions, only: [:new, :create, :show, :destroy]
+  get 'session/new'
   root to: 'tasks#index'
   resources :tasks do
     collection do
