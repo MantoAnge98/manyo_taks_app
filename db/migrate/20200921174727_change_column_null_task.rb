f048d5da81fc048707a1f5aa88a64c81ea090645
Ange <angepop1998@gmail.com>
2020-09-29 16:47:40 +0000

    Add new column to change column null Deadline.

--- db/migrate/20200921174727_change_column_null_task.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnNullTask < ActiveRecord::Migration[5.2]
-  def change
-    change_column :tasks, :status, :integer, null: false
-  end
-end
--- db/migrate/20200921195114_change_collumn_null_tasks.rb
+++
@@ -1,5 +0,0 @@
-class ChangeCollumnNullTasks < ActiveRecord::Migration[5.2]
-  def change
-    change_column :tasks, :priority, :integer, null: false
-  end
-end
