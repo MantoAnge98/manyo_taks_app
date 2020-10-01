a6f45d9d5b744b2a4766a5ce0097c66821bb8765
Ange <angepop1998@gmail.com>
2020-10-01 19:14:48 +0000

    Add Spec User

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -25,5 +25,17 @@ RSpec.describe 'User registration, login and logout functions', type: :system do
     end
   end
 
+  describe 'Testing the Session Functionality' do
+    context 'If the user has no data and is not logged in' do
+      it 'Testing Session Logins' do
+        visit new_session_path
+        fill_in 'session[email]', with: @user.email
+        fill_in 'session[password]', with: @user.password
+        click_button 'Log in'
+        expect(page).to have_content 'user@gmail.com'
+      end
+    end
+  end
+
   
 end
