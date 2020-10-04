402ec2df0d5b52dd8b98fc93ff6f29a76521fdb7
Ange <angepop1998@gmail.com>
2020-10-04 15:18:56 +0000

    Add content to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
-#2.times do |n|
-#  name = Faker::Games::Pokemon.name
-#  email = Faker::Internet.email
-#  admin = 1
-#  password = "password"
-#  User.create!(name: name,
-#               email: email,
-#               admin: "true",
-#               password: password,
-#               password_confirmation: password,
-#               )
-#end
+10.times do |n|
+  name = Faker::Games::Pokemon.name
+  email = Faker::Internet.email
+  password = "000000"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
+
+Label.create([name:'bootstrap', user_id:'1'])
+Label.create([name:'apach', user_id:'1'])
+Label.create([name:'css', user_id:'2'])
+Label.create([name:'rails', user_id:'3'])
+Label.create([name:'AR', user_id:'4'])
+Label.create([name:'IA', user_id:'5'])
+Label.create([name:'php', user_id:'6'])
+Label.create([name:'OrO', user_id:'7'])
+Label.create([name:'Data', user_id:'8'])
+Label.create([name:'gant', user_id:'9'])
